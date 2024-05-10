7d348383f30d59d49916b6cec09b54b29741f476
Timchik-IT <timur.minullin535@gmail.com>
2024-05-10 22:34:58 +0300

    Add HomeWork4 and 5-

---
+++ HomeWorks5-/TeamHostApp/TeamHostApp.WEB/Program.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Localization;
+using TeamHost.Application.Extensions;
+using TeamHost.Application.Interfaces;
+using TeamHost.Infrastructure.Extensions;
+using TeamHost.Persistence.Extensions;
+using TeamHostApp.WEB.Controllers;
+using TeamHostApp.WEB.Hub;
+using TeamHostApp.WEB.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+builder.Services
+    .AddApplicationLayer()
+    .AddInfrastructureLayer()
+    .AddPersistenceLayer(builder.Configuration);
+
+builder.Services.AddScoped<IStringLocalizer<HomeController>, HomeLocalizer>();
+builder.Services.AddSignalR()
+    .Services.AddSingleton<IHubService, HubService>();
+
+// builder.Services.AddAuthentication(options =>
+// {
+//     options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//     options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//     options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//
+// });
+
+var app = builder.Build();
+
+var supportedCultures = new[] { "en", "ru" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapHub<ChatHub>("/chat");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
